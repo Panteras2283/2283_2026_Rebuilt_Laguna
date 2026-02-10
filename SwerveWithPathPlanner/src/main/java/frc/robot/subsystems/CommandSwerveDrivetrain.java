4dfbc50215a5dea930aa4757f6deffa950b04418
rickgroc <ricardo_guerrero@panterasup.com>
2026-02-09 19:48:55 -0600

    Brownout protection
    Logic for current limit altered in CommandSwerveDrivetrain in order to add a Brownout protection.

--- SwerveWithPathPlanner/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ SwerveWithPathPlanner/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -30,6 +30,7 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj.RobotController;
 
 import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
 
@@ -51,6 +52,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private static final Rotation2d kRedAlliancePerspectiveRotation = Rotation2d.k180deg;
     /* Keep track if we've ever applied the operator perspective before or not */
     private boolean m_hasAppliedOperatorPerspective = false;
+
+    public static Current kSlipCurrent = Amps.of(120);
+
+    double voltage = 0;
 
     /** Swerve request to apply during robot-centric path following */
     private final SwerveRequest.ApplyRobotSpeeds m_pathApplyRobotSpeeds = new SwerveRequest.ApplyRobotSpeeds();
@@ -204,7 +209,14 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             });
         }
 
-        
+        voltage = RobotController.getBatteryVoltage();
+
+        if(voltage <= 10.5){
+            kSlipCurrent = Amps.of(60);
+        }
+        else{
+            kSlipCurrent = Amps.of(120);
+        }
     }
 
     private void startSimThread() {
