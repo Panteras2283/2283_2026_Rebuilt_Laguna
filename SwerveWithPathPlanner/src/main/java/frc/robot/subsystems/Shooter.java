411a264f88cdc905e5550e013d7d16b603b13a8d
rickgroc <ricardo_guerrero@panterasup.com>
2026-02-12 17:17:17 -0600

    .

--- SwerveWithPathPlanner/src/main/java/frc/robot/subsystems/Shooter.java
+++ SwerveWithPathPlanner/src/main/java/frc/robot/subsystems/Shooter.java
@@ -15,17 +15,19 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.SparkBase.ControlType;
+import frc.robot.commands.Warm_up;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Utils.ShootingTables;
 
-public class ShooterSubsystem extends SubsystemBase {
+public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
 
-  private final TalonFX Flywheel;
-  private final TalonFX Flywheel2;
+  private final TalonFX FlywheelRight;
+  private final TalonFX FlywheelLeft;
+  private double currentRPM = 0;
 
 
 
@@ -34,15 +36,17 @@ public class ShooterSubsystem extends SubsystemBase {
   private String Shooter;
 
   private double flywheelTargetRPM = 0;
+
+  
   
   private static final double RPM_TOLERANCE_PERCENT = 0.03;
 
-  public ShooterSubsystem(int flywheelCanID, int flywheelCanID2, String Shooter) {
+  public Shooter(int flywheelCanID, int flywheelCanID2, String Shooter) {
     this.Shooter = Shooter;
 
-    Flywheel = new TalonFX(flywheelCanID, "rio");
+    FlywheelRight = new TalonFX(flywheelCanID, "rio");
     configureFlywheel();
-    Flywheel2 = new TalonFX(flywheelCanID2, "rio");
+    FlywheelLeft = new TalonFX(flywheelCanID2, "rio");
     configureFlywheel2();
     
   }
@@ -54,8 +58,8 @@ public class ShooterSubsystem extends SubsystemBase {
     cfg.Slot0.kV = 0.12;
     cfg.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
-    Flywheel.getConfigurator().apply(cfg);
-    Flywheel.setNeutralMode(NeutralModeValue.Coast);
+    FlywheelRight.getConfigurator().apply(cfg);
+    FlywheelRight.setNeutralMode(NeutralModeValue.Coast);
     }
 
     private void configureFlywheel2(){
@@ -65,33 +69,44 @@ public class ShooterSubsystem extends SubsystemBase {
     cfg2.Slot0.kP = 0.1;
     cfg2.Slot0.kV = 0.12;
 
-    Flywheel2.getConfigurator().apply(cfg2);
-    Flywheel2.setNeutralMode(NeutralModeValue.Coast);
-    Flywheel2.setControl(new Follower(Constants.Shooter.motorID, MotorAlignmentValue.Opposed));
+    FlywheelLeft.getConfigurator().apply(cfg2);
+    FlywheelLeft.setNeutralMode(NeutralModeValue.Coast);
+    FlywheelLeft.setControl(new Follower(Constants.Shooter.motorID, MotorAlignmentValue.Opposed));
     }
+
+  public void setTargetRPM(double targetRPM, boolean forcePrecise){
+    double targetRPS = targetRPM/60.0;
+    if(forcePrecise){
+      FlywheelRight.setControl(flywheelRequest.withVelocity(targetRPS));
+    }else{
+      if(currentRPM > targetRPM + 100){
+        FlywheelRight.set(0);
+      }
+    }
+  }
 
   public void setTargetDistance(double distanceMeters){
     flywheelTargetRPM = ShootingTables.FlywheelMap.get(distanceMeters);
 
-    if(flywheelTargetRPM > 100){ 
+    if (flywheelTargetRPM > 100){ 
       double targetRPS = flywheelTargetRPM / 60;
-      Flywheel.setControl(flywheelRequest.withVelocity(targetRPS));
+      FlywheelRight.setControl(flywheelRequest.withVelocity(targetRPS));
     } else{
       stop();
     }
   }
 
   public void stop(){
-    Flywheel.stopMotor();
+    FlywheelRight.stopMotor();
     flywheelTargetRPM = 0;
   }
 
   public boolean isReadyToFire(){
-    double flywheelVelRPM = Flywheel.getVelocity().getValueAsDouble() * 60;
+    double flywheelVelRPM = FlywheelRight.getVelocity().getValueAsDouble() * 60;
 
     double flywheelError = Math.abs(flywheelTargetRPM - flywheelVelRPM);
 
-    boolean flywheelReady = flywheelError < (flywheelTargetRPM * RPM_TOLERANCE_PERCENT);
+    boolean flywheelReady =  flywheelError < (flywheelTargetRPM * RPM_TOLERANCE_PERCENT);
 
     return flywheelReady && (flywheelTargetRPM > 100);
   }
@@ -99,9 +114,11 @@ public class ShooterSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  double currentRPM = FlywheelRight.getVelocity().getValueAsDouble()*60;
 
+  SmartDashboard.putNumber(Shooter + "/FlywheelRightRPM", FlywheelRight.getVelocity().getValueAsDouble() * 60.0);
+  SmartDashboard.putNumber(Shooter + "/FlywheelLeftRPM", FlywheelRight.getVelocity().getValueAsDouble() * 60.0);
 
-  SmartDashboard.putNumber(Shooter + "/FlywheelRPM", Flywheel.getVelocity().getValueAsDouble() * 60.0);
   SmartDashboard.putBoolean(Shooter + "/Ready", isReadyToFire());
   }
 }
