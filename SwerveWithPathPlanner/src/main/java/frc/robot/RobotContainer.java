7ccef6239c4a55f9817f42daf0f76b7e3c004463
Santiago Sepúlveda <60012979+Sepu2002@users.noreply.github.com>
2026-03-04 16:08:41 -0600

    .

--- SwerveWithPathPlanner/src/main/java/frc/robot/RobotContainer.java
+++ SwerveWithPathPlanner/src/main/java/frc/robot/RobotContainer.java
@@ -58,9 +58,9 @@ public class RobotContainer {
     public final TurretSubsystem s_Turret = new TurretSubsystem(25, "Turret");
     public final Shooter s_Shooter = new Shooter(Constants.Shooter.motorID, Constants.Shooter.motor2ID, "Shooter");
 
-    public final Superstructure superstructure = new Superstructure(s_Turret, s_Shooter,
+   /*  public final Superstructure superstructure = new Superstructure(s_Turret, s_Shooter,
     ()->drivetrain.getState().Pose,
-    ()->drivetrain.getState().Speeds, operator, s_Kicker, s_Spindexer);
+    ()->drivetrain.getState().Speeds, operator, s_Kicker, s_Spindexer);*/
     
     public final VisionSubsystem s_Vision;
     
@@ -104,7 +104,7 @@ public class RobotContainer {
             )
         );
 
-        superstructure.setDefaultCommand(new RunCommand(()->superstructure.periodic(), superstructure));
+        //superstructure.setDefaultCommand(new RunCommand(()->superstructure.periodic(), superstructure));
         s_Intake.setDefaultCommand(new IntakeDefault(s_Intake));
       // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
@@ -141,8 +141,8 @@ public class RobotContainer {
         );
 
             /*Turret */
-       operator.leftBumper().onTrue(new InstantCommand(superstructure::toggleIdle));
-       operator.rightBumper().onTrue(new InstantCommand(superstructure::toggleShooting));
+       /*operator.leftBumper().onTrue(new InstantCommand(superstructure::toggleIdle));
+       operator.rightBumper().onTrue(new InstantCommand(superstructure::toggleShooting));*/
 
             /*Intake */
        operator.pov(180).onTrue(new InstantCommand(()->s_Intake.Down()));
@@ -152,6 +152,14 @@ public class RobotContainer {
        operator.pov(0).whileTrue(s_Intake.startEnd(s_Intake::outake, s_Intake::stop));
 
        operator.pov(270).onTrue(new InstantCommand(()->s_Intake.up()));
+
+       operator.x().onTrue(new InstantCommand(()->s_Spindexer.SpinCCW()));
+       operator.x().onFalse(new InstantCommand(()->s_Spindexer.stop()));
+
+       operator.rightBumper().onTrue(new SimpleShoot(s_Shooter, s_Kicker, s_Spindexer));
+       operator.rightBumper().onFalse(new InstantCommand(()->s_Shooter.stop()));
+       operator.rightBumper().onFalse(new InstantCommand(()->s_Kicker.stop()));
+       operator.rightBumper().onFalse(new InstantCommand(()->s_Spindexer.stop()));
             
             /*Climber */
        //operator.leftStick().whileTrue(s_Climber.fullDownCommand());
---
+++ SwerveWithPathPlanner/src/main/java/frc/robot/commands/SimpleShoot.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.*;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class SimpleShoot extends Command {
+private Shooter s_Shooter;
+private Kicker s_Kicker;
+private Spindexer s_Spindexer;
+
+  /** Creates a new SimpleShoot. */
+  public SimpleShoot(Shooter s_Shooter, Kicker s_Kicker, Spindexer s_Spindexer) {
+    this.s_Shooter = s_Shooter;
+    this.s_Kicker = s_Kicker;
+    this.s_Spindexer = s_Spindexer;
+
+    addRequirements(s_Shooter, s_Kicker, s_Spindexer);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    s_Shooter.setRPM(false, 2000);
+    s_Kicker.Kick(0.5);
+    s_Spindexer.SpinCW();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
